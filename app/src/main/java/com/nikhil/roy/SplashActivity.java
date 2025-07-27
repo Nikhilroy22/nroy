d39e812d26feaec576ec66e5c996c51970434ed5
Nikhilroy22 <nikhilroyfff@gmail.com>
2025-07-27 23:40:17 +0600

    first commit

---
+++ app/src/main/java/com/nikhil/roy/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.nikhil.roy;
+
+import android.widget.*;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.lang.Thread;
+
+
+
+public class SplashActivity extends AppCompatActivity {
+  
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        
+       
+        
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+          
+          startActivity(new Intent(this, MainActivity.class));
+            finish();
+          
+          
+        }, 2500);
+        }
+        
+        
+        
+   }
+  
