63882ccefafa988d941e800ca3be5e6ac517b04c
Nikhilroy22 <nikhilroyfff@gmail.com>
2025-07-28 13:51:11 +0600

    first commit

--- app/src/main/java/com/nikhil/roy/LoginActivity.java
+++ app/src/main/java/com/nikhil/roy/LoginActivity.java
@@ -3,40 +3,88 @@ package com.nikhil.roy;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.*;
-import android.view.View;
-import android.util.Patterns;
 import android.content.Intent;
+import android.util.Patterns;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-  
-  Button signupnav, loginbutton;
-  
-  
+
+    EditText email, password;
+    Button signupnav, loginbutton;
+    FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
-        
-        //Button
+
+        // EditText
+        email = findViewById(R.id.EmailInput);
+        password = findViewById(R.id.PasswordInput);
+
+        // Button
         signupnav = findViewById(R.id.SignupLink);
         loginbutton = findViewById(R.id.LoginButton);
-        //Signup Navigation
+
+        // Firebase
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // Signup Navigation
         signupnav.setOnClickListener(v -> {
-          startActivity(new Intent(this, SignupActivity.class));
-            
-          
+            startActivity(new Intent(this, SignupActivity.class));
         });
-        //FIREBASE LOGIN Button
+
+        // Login Button
         loginbutton.setOnClickListener(v -> {
-          login();
-          
+            String Semail = email.getText().toString().trim();
+            String Spassword = password.getText().toString().trim();
+
+            // Validation
+            if (!Patterns.EMAIL_ADDRESS.matcher(Semail).matches()) {
+                email.setError("সঠিক ইমেইল দিন");
+                email.requestFocus();
+                return;
+            }
+            if (Spassword.length() < 6) {
+                password.setError("পাসওয়ার্ড ৬ অক্ষরের বেশি হওয়া উচিত");
+                password.requestFocus();
+                return;
+            }
+
+            // Login function call
+            login(Semail, Spassword);
         });
-        
     }
-    //LOGIN FUNCTION
-    public void login(){
-      
-      Toast.makeText(this, "সাইনআপ ব্যর্থ: " , Toast.LENGTH_LONG).show();
-    }
+
+    // Login Function
+    public void login(String email, String password) {
+    firebaseAuth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(this, "লগইন সফল", Toast.LENGTH_SHORT).show();
+            } else {
+                String error = task.getException().getMessage();
+
+                if (error.contains("The supplied")) {
+                  Toast.makeText(this, "Invalid Email/Password", Toast.LENGTH_LONG).show();
+                  
+                } else {
+                    Toast.makeText(this, "লগইন ব্যর্থ: " + error, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+}
+
+    // Auto login check
+   /* @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if (currentUser != null) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+    }*/
 }
